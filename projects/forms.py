7df827b28d8780d9da647a23a294b029f12eb2e4
dborysenko <borisen@forever.org.ua>
2015-09-29 12:41:45 +0300

    Added django-bootstrap lib to work with forms

--- projects/forms.py
+++ projects/forms.py
@@ -4,6 +4,6 @@ from .models import Projects
 
 class CreateProjectForm(forms.Form):
     name = forms.CharField(max_length=255)
-    description = forms.CharField(max_length=255)
+    description = forms.CharField(max_length=255, required=False)
     directory = forms.CharField(max_length=255)
 
--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.urlresolvers import reverse
 
 
 class Projects(models.Model):
