3511f133fb51f958f3b97c5a737280bd65ed60c8
borisensx <borisen@forever.org.ua>
2015-09-30 22:48:36 +0300

    fixed navbar javascript active buttons

--- static_in_pro/our_static/js/ansibleTower.js
+++ static_in_pro/our_static/js/ansibleTower.js
@@ -2,15 +2,10 @@
  * Created by dborysenko on 9/30/2015.
  */
 $(document).ready(function () {
-    var pathname = window.location.pathname;
-    console.log(pathname)
-    if (pathname.indexOf('projects') == 1) {
-        $('#Projects').attr('class', 'active')
-    }else if (pathname.indexOf('inventories') == 1){
-        $('#Inventories').attr('class', 'active')
-    }else if (pathname.indexOf('jobtemplates') == 1){
-        $('#JobTemplates').attr('class', 'active')
+    var pathname = window.location.pathname.split('/')[1];
+    if (pathname == ''){
+        $('#home').attr('class', 'active')
     }else{
-        $('#Home').attr('class', 'active')
+        $('#' + pathname).attr('class', 'active')
     }
 });
